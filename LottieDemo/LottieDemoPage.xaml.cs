d2507071b2475f5dbaf12b1093712c59b6778153
Paul O Donnell <podonne5@gmail.com>
2017-03-06 11:09:20 +0000

    more animations

--- LottieDemo/LottieDemoPage.xaml.cs
+++ LottieDemo/LottieDemoPage.xaml.cs
@@ -9,10 +9,19 @@ namespace LottieDemo
             InitializeComponent();
 
             //lottieAnim.WidthRequest = 100;
+
+            //@podonnell: This is needed to stop an error occuring when running the LottieLogo1.json animation
+            //LottieAnimationView not displayed because it is too large to fit into a software layer (or drawing cache)
             lottieAnim.HeightRequest = 1;
 
             lottieAnim.VerticalOptions = LayoutOptions.FillAndExpand;
             lottieAnim.HorizontalOptions = LayoutOptions.FillAndExpand;
+
+            lottieAnim.Animation = "LottieDemoAnim.json";
+            //lottieAnim.Animation = "hamburger.json";
+            //lottieAnim.Animation = "LottieLogo1.json";
+            lottieAnim.Loop = true;
+            lottieAnim.AutoPlay = true;
 
             startStopBtn.Clicked += (object sender, System.EventArgs e) => 
             {
